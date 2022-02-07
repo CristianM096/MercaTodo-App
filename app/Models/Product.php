610b30878f1818218d3f2a9e206a32db9c2277fa
Chriztian Manzano <cristianorb@unicauca.edu.co>
2022-02-06 20:09:22 -0500

    feat: add field image for product and methods get

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Facade\Ignition\QueryRecorder\Query;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -17,6 +19,49 @@ class Product extends Model
         'stock',
         'color',
         'weight',
-        'size'
+        'size',
+        'active',
     ];
+    
+    public function getPhotoAttribute(){
+        $name = $this->getImageName();
+        return url('/storage/productImages/'.$name);
+    }
+    public function getImageName(){
+        $path = explode("\\", $this->attributes['photo']);
+        if(sizeof($path) === 2){
+            $name = $path[1];
+        }else{
+            $name = $path[0];
+        }
+        return $name;
+    }
+    public static function scopeName(Builder $query, ? string $name):Builder
+    {
+        if(null !== $name){
+            $query->where('name','like', "%$name%");
+            return $query;
+        }
+        return $query;
+    }
+    public static function scopePriceMax(Builder $query, ? float $priceMax):Builder
+    {
+        if(null === $priceMax || 0>=$priceMax){
+            return $query;
+        }else{
+            $query->where('price','<', $priceMax);
+            return $query;
+        }
+    }
+
+    public static function scopePriceMin(Builder $query, ? float $priceMin):Builder
+    {
+        if(null === $priceMin || 0 > $priceMin){
+            return $query;
+        }else{
+            //dd($priceMax);
+            $query->where('price',">", $priceMin);
+            return $query;
+        }
+    }
 }
